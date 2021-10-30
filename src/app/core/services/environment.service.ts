bd5a9b29f9125de00060fdbe7e936a54b900ebee
Stephanie Tuerk <stephanie.tuerk@gmail.com>
2021-10-30 14:18:05 -0400

    add env service methods to determine whether dev or prod

--- src/app/core/services/environment.service.ts
+++ src/app/core/services/environment.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environmentConstants } from '../constants/environment.constants';
 import { Environments, EnvironmentSettings } from '../models/environments.model';
+import { Environment, EnvironmentOption } from '../models/project.model';
 
 @Injectable({
     providedIn: 'root',
@@ -8,6 +9,9 @@ import { Environments, EnvironmentSettings } from '../models/environments.model'
 export class EnvironmentService {
     environment: Environments;
     environmentSettings: EnvironmentSettings;
+    localStrings: string[] = ['localhost'];
+    productionStrings: string[] = ['personal-site-c3c69', 'stephanietuerk'];
+    errorMessage: 'Cannot determine environment';
 
     constructor() {
         this.getSettings();
@@ -16,14 +20,36 @@ export class EnvironmentService {
     private getSettings(): void {
         const origin = window.location.href;
         this.environment = environmentConstants;
-        if (origin.indexOf('localhost') > -1) {
+        if (this.stringInArray(origin, this.localStrings)) {
             this.environmentSettings = this.environment.local;
-        } else if (origin.indexOf('personal-site-c3c69') > -1) {
-            this.environmentSettings = this.environment.production;
-        } else if (origin.indexOf('stephanietuerk') > -1) {
+        } else if (this.stringInArray(origin, this.productionStrings)) {
             this.environmentSettings = this.environment.production;
         } else {
-            console.error('Unknown Environment');
+            console.error(this.errorMessage);
+        }
+    }
+
+    private isLocal(): boolean {
+        const origin = window.location.href;
+        return this.stringInArray(origin, this.localStrings);
+    }
+
+    private isProduction(): boolean {
+        const origin = window.location.href;
+        return this.stringInArray(origin, this.productionStrings);
+    }
+
+    private stringInArray(str: string, arr: string[]): boolean {
+        return arr.some((x) => str.match(new RegExp(x, 'gi')));
+    }
+
+    getEnvironment(): EnvironmentOption {
+        if (this.isLocal()) {
+            return Environment.local;
+        } else if (this.isProduction()) {
+            return Environment.production;
+        } else {
+            console.error(this.errorMessage);
         }
     }
 }
