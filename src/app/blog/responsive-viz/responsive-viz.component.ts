dc69e7cdab31dd3ad368c59b5b2f115926db3803
Stephanie Tuerk <stephanie.tuerk@gmail.com>
2021-10-30 14:20:07 -0400

    add reponsive viz project

---
+++ src/app/blog/responsive-viz/responsive-viz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-responsive-viz',
+  templateUrl: './responsive-viz.component.html',
+  styleUrls: ['./responsive-viz.component.scss']
+})
+export class ResponsiveVizComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
